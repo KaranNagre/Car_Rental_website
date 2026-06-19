59319402a7f51db315b60bd89f18063dc10617af
karan nagre <karannagre143@gmail.com>
2026-06-19 09:47:43 +0530

    Dark mode js

--- main.js
+++ main.js
@@ -142,3 +142,22 @@ search.addEventListener("keyup", () => {
     card.style.display = carName.includes(value) ? "block" : "none";
   });
 });
+
+const themeBtn = document.getElementById("theme-btn");
+
+if (localStorage.getItem("theme") === "dark") {
+  document.body.classList.add("dark");
+  themeBtn.textContent = "☀️";
+}
+
+themeBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+
+  if (document.body.classList.contains("dark")) {
+    localStorage.setItem("theme", "dark");
+    themeBtn.textContent = "☀️";
+  } else {
+    localStorage.setItem("theme", "light");
+    themeBtn.textContent = "🌙";
+  }
+});
